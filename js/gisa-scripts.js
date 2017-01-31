ed5671b2fff40c78d54f1f7bb5856593f4c2b0d4
Antonio Leutsch <post@antonio-leutsch.com>
2017-01-31 16:04:15 +0100

    Changes

--- js/gisa-scripts.js
+++ js/gisa-scripts.js
@@ -23,15 +23,15 @@ var SGB = window.SGB || {};
     var queryAll = document.querySelectorAll.bind(document);
 
     Node.prototype.on = window.on = function (name, fn) {
-      this.addEventListener(name, fn)
+      this.addEventListener(name, fn);
     };
 
     NodeList.prototype.forEach = Array.prototype.forEach;
 
     NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn) {
       this.forEach(function (elem, i) {
-        elem.on(name, fn)
-      })
+        elem.on(name, fn);
+      });
     };
 
      // Add functionality to toggle classes on elements
@@ -92,3 +92,62 @@ var SGB = window.SGB || {};
     queryAll('.gisa-btn--select').on('click', SGB.selectSourceCode);
   }
 }(this, SGB));
+
+
+function scroller() {
+
+    var jQuerysidebar   = jQuery("#scroll"),
+        jQuerywindow    = jQuery(window),
+        offset     = jQuerysidebar.offset(),
+        topPadding = 15;
+
+    jQuerywindow.scroll(function() {
+        if (jQuerywindow.scrollTop() > offset.top) {
+            jQuerysidebar.stop().animate({
+                marginTop: jQuerywindow.scrollTop() - offset.top + topPadding
+            }, 500);
+        } else {
+            jQuerysidebar.stop().animate({
+                marginTop: 0
+            });
+        }
+    });
+
+}
+
+jQuery(document).ready(function() {
+  scroller();
+	jQuery('.owl-carousel').owlCarousel({
+		loop: true,
+		margin: 10,
+		nav: true,
+    navText: ["<span class='nav left'><i class='arrow_carrot-left'></i></span>","<span class='nav right'><i class='arrow_carrot-right'></i></span>"],
+    navElement: 'span',
+    dots: false,
+		items: 1,
+	});
+  document.getElementById("defaultOpen").click();
+  document.getElementById("defaultOpen").removeAttribute('id');
+});
+
+
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
