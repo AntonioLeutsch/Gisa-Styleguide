dab8fc804643cd3fa5f5770078877dba42cddd4e
Antonio Leutsch <post@antonio-leutsch.com>
2017-02-02 15:45:13 +0100

    ACC

--- js/gisa-scripts.js
+++ js/gisa-scripts.js
@@ -93,86 +93,113 @@ var SGB = window.SGB || {};
   }
 }(this, SGB));
 
-
 function coolFunctions() {
-  if ($(window).width() <= 1024) {
-
-    // Scroll
-    $('#scroll').trigger('sticky_kit:detach');
-
-    //Menu
-
-
-  } else {
-
-    //Scroll
-    $('#scroll').stick_in_parent();
-
-    //Menu
-    var elm = document.querySelector('#scroll');
-    var ms  = new MenuSpy(elm, {
-      activeClass: 'current-item'
+	if ($(window).width() <= 1024) {
+		// Scroll
+		$('#scroll').trigger('sticky_kit:detach');
+	} else {
+		//Scroll
+		$('#scroll').stick_in_parent();
+		//Menu
+		var elm = document.querySelector('#scroll');
+		var ms = new MenuSpy(elm, {
+      activeClass : 'current-item'
     });
-  }
+	}
 }
+
+function counter() {
+	jQuery('.timer').countTo({
+    from: 0,
+    to: 650
+  });
+}
+
+
+var X = true;
+$(function() {
+  if (X == true) {
+
+    console.log(X);
+    var oTop = $('.timer').offset().top - window.innerHeight;
+    $(window).scroll(function(){
+
+        var pTop = $('body').scrollTop();
+        //console.log( pTop + ' - ' + oTop );   //just for your debugging
+        if( pTop > oTop  && X != false){
+            counter();
+            X = false;
+          }
+      });
+    }
+});
+
 
 function myAccordion() {
 
-  jQuery('.accordion dt').bind('click', function () {
-    jQuery(this).siblings('dd.active').slideToggle('fast');
-    jQuery(this).parent().find('.active').removeClass('active');
-    jQuery(this).addClass('active');
-    jQuery(this).next('dd').slideToggle('fast').addClass('active');
+	jQuery('.accordion dt').bind('click', function() {
+		if (!jQuery(this).hasClass('active')) {
 
-    // Toggle Icon
-    jQuery(this).find('i').removeClass('icon_plus_alt2');
-    jQuery(this).find('i').addClass('icon_minus_alt');
-    jQuery(this).parent().find('dt').not('.active').find('i').removeClass('icon_minus_alt');
-    jQuery(this).parent().find('dt').not('.active').find('i').addClass('icon_plus_alt2');
+			jQuery(this).siblings('dd.active').slideToggle('fast');
+			jQuery(this).parent().find('.active').removeClass('active');
+			jQuery(this).addClass('active');
+			jQuery(this).next('dd').slideToggle('fast').addClass('active');
 
-  });
-  jQuery('.accordion #open').click();
+			// Toggle Icon
+			jQuery(this).find('i').removeClass('icon_plus_alt2');
+			jQuery(this).find('i').addClass('icon_minus_alt');
+			jQuery(this).parent().find('dt').not('.active').find('i').removeClass('icon_minus_alt');
+			jQuery(this).parent().find('dt').not('.active').find('i').addClass('icon_plus_alt2');
+
+		}
+	});
+	jQuery('.accordion #open').click();
 }
 
 
 jQuery(document).ready(function() {
-  myAccordion();
-  coolFunctions();
+  //counter();
+	myAccordion();
+	coolFunctions();
 	jQuery('.owl-carousel').owlCarousel({
 		loop: true,
 		margin: 10,
 		nav: true,
-    navText: ["<span class='nav left'><i class='arrow_carrot-left'></i></span>","<span class='nav right'><i class='arrow_carrot-right'></i></span>"],
-    navElement: 'span',
-    dots: false,
-		items: 1,
+		navText: [
+			"<span class='nav left'><i class='arrow_carrot-left'></i></span>", "<span class='nav right'><i class='arrow_carrot-right'></i></span>",
+		],
+		navElement: 'span',
+		dots: false,
+		items: 1
 	});
-  document.getElementById("defaultOpen").click();
-  document.getElementById("defaultOpen").removeAttribute('id');
+	document.getElementById("defaultOpen").click();
+	document.getElementById("defaultOpen").removeAttribute('id');
 });
 
-
-jQuery(window).on('resize', function(){
-  coolFunctions();
+jQuery(window).on('resize', function() {
+	coolFunctions();
+  X = true;
 });
 
 function openTab(evt, tabName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
+	// Declare all variables
+	var i,
+		tabcontent,
+		tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
 
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+	// Show the current tab, and add an "active" class to the link that opened the tab
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
 }
